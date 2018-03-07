24afe362f5f5861bb0403fe56f4ab53c08d55c92
B2T\bruno.costa <bruno.costa@b2t.com.br>
2018-03-07 13:59:42 -0300

    Scala Tuturial ++

---
+++ src/main/scala/ScalaTuturial/ImperativeProgramming.scala
@@ -0,0 +1,46 @@
+package ScalaTuturial
+
+class BankAccount {
+  private var balance = 0
+  def deposit(amount: Int): Int = {
+    if (amount > 0) balance = balance + amount
+    balance
+  }
+  def withdraw(amount: Int): Int =
+    if (0 < amount && amount <= balance) {
+      balance = balance - amount
+      balance
+    } else throw new Error("insufficient funds")
+}
+
+
+object BankAccount extends App{
+
+  val account = new BankAccount // account: BankAccount = BankAccount
+  //account deposit 50 //
+  //account withdraw 20 // res1: Int = 30
+  //account.withdraw(20) // res2: Int = 10
+  //account withdraw 15                 // java.lang.Error: insufficient funds
+
+  val x = new BankAccount
+  val y = new BankAccount
+
+  println(x deposit 30)
+  //println(y withdraw 20)                // java.lang.Error: insufficient funds
+
+  def factorial(n: Int): Int = {
+    var result =  1
+
+    var i =  1
+
+    while (i <= n) {
+      result = result * i
+      i = i + 1
+
+    }
+    result
+  }
+
+  println(factorial(5))
+}
+
